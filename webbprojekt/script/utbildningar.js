36e9b8b375486c818f2816f4d507bc6e106783a4
Lars Stromberg <stromberg.lars@gmail.com>
2018-11-21 09:17:26 +0100

    Justerat jQuery
    Justerat jQuery på utbildningar men inte helt klart. Skapar faq från utbildningar som mall.

--- webbprojekt/script/utbildningar.js
+++ webbprojekt/script/utbildningar.js
@@ -1,9 +1,9 @@
-$( function() {
-    $( "#accordion" ).accordion();
-} );
+$( function() { $( "#accordion" ).accordion(); });
 
-$(".imageBig, #courseTextIt").hover(function(){
-    $('#courseTextIt').show();
-},function(){
-    $('#courseTextIt').hide();
-});
+// snygga till med this om tid finnes.
+$(".imageBig1, #courseText1").hover(function(){ $('#courseText1').show(); },function(){ $('#courseText1').hide(); });
+$(".imageBig2, #courseText2").hover(function(){ $('#courseText2').show(); },function(){ $('#courseText2').hide(); });
+$(".imageBig3, #courseText3").hover(function(){ $('#courseText3').show(); },function(){ $('#courseText3').hide(); });
+$(".imageBig4, #courseText4").hover(function(){ $('#courseText4').show(); },function(){ $('#courseText4').hide(); });
+$(".imageBig5, #courseText5").hover(function(){ $('#courseText5').show(); },function(){ $('#courseText5').hide(); });
+$(".imageBig6, #courseText6").hover(function(){ $('#courseText6').show(); },function(){ $('#courseText6').hide(); });
