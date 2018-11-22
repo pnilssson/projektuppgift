4d8b8c354c89001c6aae394aefc73faae0d21f2e
Lars Stromberg <stromberg.lars@gmail.com>
2018-11-22 08:34:54 +0100

    Tagit bort utbildningar2
    Behövs ej

--- webbprojekt/script/utbildningar.js
+++ webbprojekt/script/utbildningar.js
@@ -1,4 +1,4 @@
-$( function() { $( "#accordion" ).accordion(); });
+$( function() { $( "#accordion" ).accordion({collapsible: true, heightStyle: "content"}); });
 
 // snygga till med this om tid finnes.
 $(".imageBig1, #courseText1").hover(function(){ $('#courseText1').show(); },function(){ $('#courseText1').hide(); });
