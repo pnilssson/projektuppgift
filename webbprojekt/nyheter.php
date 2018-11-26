9cafc32fa3c66dbbacfcc2e6534d954a38b2dd84
Anna Amberntsson <amberntsson.anna@gmail.com>
2018-11-26 11:10:02 +0100

    ny php.fil för nyheter

---
+++ webbprojekt/nyheter.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Campus Mölndal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+<nav class="navbar">
+    <div class="mobile-logo"></div>
+    <div class="navbar-toggle" id="js-navbar-toggle">
+        <i class="fas fa-bars"></i>
+    </div>
+    <ul id="js-navbar">
+        <li><a href="#"><i class="far fa-compass icon"></i>Studie- och yrkesväglednig</a></li>
+        <li><a href="#"><i class="fas fa-book icon"></i>Dina studier</a></li>
+        <li><a href="#" class="desktop-logo"></a></li>
+        <li><a href="#"><i class="far fa-building icon"></i>Företag och organisationer</a></li>
+        <li><a href="#"><i class="fas fa-mobile-alt icon"></i>Kontakta oss</a></li>
+    </ul>
+</nav>
+
+<footer class="footer">
+    <ul>
+        <h4>Kontakt</h4>
+        <li class="footer-item"><a href="mailto:vagledning.campus@molndal.se"><i class="far fa-envelope"></i>Vägledning</a></li>
+        <li class="footer-item"><a href="tel:031-315 10 14"><i class="fas fa-phone"></i>031-315 10 14</a></li>
+        <li class="footer-item"><a href="https://www.facebook.com/campusmolndal/"><i class="fab fa-facebook-f"></i>Facebook</a> </li>
+    </ul>
+    <ul>
+        <h4>Hitta oss</h4>
+        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">Krokslätts Fabriker 12A</li></a>
+        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">431 37</li></a>
+        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">Mölndal</li></a>
+    </ul>
+    <ul>
+        <h4>Snabblänkar</h4>
+        <a href="#"><li class="footer-item">Ansökan</li></a>
+        <a href="https://campusmolndal.itslearning.com/"><li class="footer-item">Itslearning</li></a>
+        <a href="#"><li class="footer-item">Felanmälan</li></a>
+        <a href="https://www.molndal.se/startsida/kommun-och-politik/insyn-och-paverkan/synpunkter-och-felanmalan.html"><li class="footer-item">Lediga jobb</li></a>
+    </ul>
+</footer>
+<script src="script/navbar-toggle.js"></script>
+</body>
+</html>
+
+
+<?php
+?>
