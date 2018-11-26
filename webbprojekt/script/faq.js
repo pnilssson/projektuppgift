9a4be4c32b3f06c9990a389b08d84a94b7302a91
Lars Stromberg <stromberg.lars@gmail.com>
2018-11-26 14:29:38 +0100

    Justeringar

--- webbprojekt/script/faq.js
+++ webbprojekt/script/faq.js
@@ -3,17 +3,34 @@ $( function() { $( ".accordion" ).accordion({
     heightStyle: "content"
 }); });
 
-$( "#almannaFragor" ).on( "click", function() {
-    $("almannaFragor").show(),
-    $("yhAccordion").hide(),
-    $("skolanAccordion").hide(),
-    $("csnAccordion").hide(),
-    $("csnAccordion").hide()
-});
-$( "#yh" ).on( "click", function() {
-    $("almannaFragor").hide,
-        $("yhAccordion").show(),
-        $("skolanAccordion").hide(),
-        $("csnAccordion").hide(),
-        $("csnAccordion").hide()
-});
+// $( "#almannaFragor" ).on( "click", function() {
+//     $("almannaFragor").show(),
+//     $("yhAccordion").hide(),
+//     $("skolanAccordion").hide(),
+//     $("csnAccordion").hide(),
+//     $("csnAccordion").hide()
+// });
+// $( "#yh" ).on( "click", function() {
+//     $("almannaFragor").hide,
+//         $("yhAccordion").show(),
+//         $("skolanAccordion").hide(),
+//         $("csnAccordion").hide(),
+//         $("csnAccordion").hide()
+// });
+
+$( "#almannaFragor" ).click(function () {
+    alert("hide")
+    // $("almannaFragorAccordion").hide(),
+    // $("yhAccordion").show(),
+    // $("skolanAccordion").hide(),
+    // $("csnAccordion").hide(),
+    // $("csnAccordion").hide()
+})
+$( "#yh" ).click(function () {
+    alert("show")
+    // $("almannaFragorAccordion").show(),
+    // $("yhAccordion").show(),
+    // $("skolanAccordion").hide(),
+    // $("csnAccordion").hide(),
+    // $("csnAccordion").hide()
+})
