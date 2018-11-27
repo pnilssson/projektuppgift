7fc6eb70a8614fad6f21e115267e4a73ae2f28c0
Anna Amberntsson <amberntsson.anna@gmail.com>
2018-11-27 10:34:09 +0100

    ändrat php-koden på nyhetssidan

--- webbprojekt/nyheter.php
+++ webbprojekt/nyheter.php
@@ -35,20 +35,22 @@
 
 
 <?php
-/*
 require_once("db_connect.php");
 
 $connect = dbConnect();
 
 $sql = "SELECT * FROM news ORDER BY newsId DESC";
 $result = mysqli_query($connect, $sql);
- while ($row = mysqli_fetch_assoc($result)){
+$i = 1;
+for($j = 0; $j < 9; $j++){
+    $row = mysqli_fetch_assoc($result);
     echo "<article class='grid-news-$i'>";
     echo "<img src='img/newsImages/".$row['newsImg']."'>";
     echo "<h4>".$row['newsTitle']."</h4>";
     echo "<p class='news-text'>".$row['newsText']."</p>";
     echo "</article>";
-} */
+    $i++;
+}
 ?>
 
 <footer class="footer">
