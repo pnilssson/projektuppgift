e309585176a8d39d8fc014a88e607aa2e019e072
vinaren6 <viktor.ruden2@gmail.com>
2018-11-27 10:22:08 +0100

    Alla sidor läser in footer via json

--- webbprojekt/nyheter.php
+++ webbprojekt/nyheter.php
@@ -51,27 +51,11 @@ $result = mysqli_query($connect, $sql);
 } */
 ?>
 
-<footer class="footer">
-    <ul>
-        <h4>Kontakt</h4>
-        <li class="footer-item"><a href="mailto:vagledning.campus@molndal.se"><i class="far fa-envelope"></i>Vägledning</a></li>
-        <li class="footer-item"><a href="tel:031-315 10 14"><i class="fas fa-phone"></i>031-315 10 14</a></li>
-        <li class="footer-item"><a href="https://www.facebook.com/campusmolndal/"><i class="fab fa-facebook-f"></i>Facebook</a> </li>
-    </ul>
-    <ul>
-        <h4>Hitta oss</h4>
-        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">Krokslätts Fabriker 12A</li></a>
-        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">431 37</li></a>
-        <a href="https://www.google.se/maps/place/Campus+M%C3%B6lndal/@57.670819,12.0086165,15z/data=!4m2!3m1!1s0x0:0xae402e7a2c7e6dd4?ved=2ahUKEwjDn8qj4c7eAhWBxosKHQjzBfMQ_BIwCnoECAYQCA"><li class="footer-item">Mölndal</li></a>
-    </ul>
-    <ul>
-        <h4>Snabblänkar</h4>
-        <a href="#"><li class="footer-item">Ansökan</li></a>
-        <a href="https://campusmolndal.itslearning.com/"><li class="footer-item">Itslearning</li></a>
-        <a href="#"><li class="footer-item">Felanmälan</li></a>
-        <a href="https://www.molndal.se/startsida/kommun-och-politik/insyn-och-paverkan/synpunkter-och-felanmalan.html"><li class="footer-item">Lediga jobb</li></a>
-    </ul>
+<footer class="footer" id="footer">
+
 </footer>
+
+<script src="script/footer.js"></script>
 <script src="script/navbar-toggle.js"></script>
 </body>
 </html>
