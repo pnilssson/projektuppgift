53f47dc1262e13b7a5deb37ac3e5e0c4d6094735
Lars Stromberg <stromberg.lars@gmail.com>
2018-12-04 12:51:30 +0100

    Gjort klart FAQ med allt fungerande.

--- webbprojekt/script/faq.js
+++ webbprojekt/script/faq.js
@@ -1,36 +1,60 @@
+// Gör så att höjden på varje ruta anpassas efter mängden text och så att man kan kollapsa alla "titlar (h4)"
 $( function() { $( ".accordion" ).accordion({
     collapsible: true,
     heightStyle: "content"
 }); });
 
-// $( "#almannaFragor" ).on( "click", function() {
-//     $("almannaFragor").show(),
-//     $("yhAccordion").hide(),
-//     $("skolanAccordion").hide(),
-//     $("csnAccordion").hide(),
-//     $("csnAccordion").hide()
-// });
-// $( "#yh" ).on( "click", function() {
-//     $("almannaFragor").hide,
-//         $("yhAccordion").show(),
-//         $("skolanAccordion").hide(),
-//         $("csnAccordion").hide(),
-//         $("csnAccordion").hide()
-// });
+// Visa enbart "Allmänna frågor"
+$( function() {
+    $( "#almannaFragor" ).on( "click", function() {
+        $( "#almannaFragorAccordion" ).removeClass( "hiddenAccordion" );
+        $( "#yhAccordion" ).addClass( "hiddenAccordion" );
+        $( "#skolanAccordion" ).addClass( "hiddenAccordion" );
+        $( "#csnAccordion" ).addClass( "hiddenAccordion" );
+        $( "#hurDuAnsokerAccordion" ).addClass( "hiddenAccordion" );
+    });
+} );
 
-$( "#almannaFragor" ).click(function () {
-    alert("hide")
-    // $("almannaFragorAccordion").hide(),
-    // $("yhAccordion").show(),
-    // $("skolanAccordion").hide(),
-    // $("csnAccordion").hide(),
-    // $("csnAccordion").hide()
-})
-$( "#yh" ).click(function () {
-    alert("show")
-    // $("almannaFragorAccordion").show(),
-    // $("yhAccordion").show(),
-    // $("skolanAccordion").hide(),
-    // $("csnAccordion").hide(),
-    // $("csnAccordion").hide()
-})
+// Visa enbart "YH"
+$( function() {
+    $( "#yh" ).on( "click", function() {
+        $( "#almannaFragorAccordion" ).addClass( "hiddenAccordion" );
+        $( "#yhAccordion" ).removeClass( "hiddenAccordion" );
+        $( "#skolanAccordion" ).addClass( "hiddenAccordion" );
+        $( "#csnAccordion" ).addClass( "hiddenAccordion" );
+        $( "#hurDuAnsokerAccordion" ).addClass( "hiddenAccordion" );
+    });
+} );
+
+// Visa enbart "Skolan"
+$( function() {
+    $( "#skolan" ).on( "click", function() {
+        $( "#almannaFragorAccordion" ).addClass( "hiddenAccordion" );
+        $( "#yhAccordion" ).addClass( "hiddenAccordion" );
+        $( "#skolanAccordion" ).removeClass( "hiddenAccordion" );
+        $( "#csnAccordion" ).addClass( "hiddenAccordion" );
+        $( "#hurDuAnsokerAccordion" ).addClass( "hiddenAccordion" );
+    });
+} );
+
+// Visa enbart "CSN"
+$( function() {
+    $( "#csn" ).on( "click", function() {
+        $( "#almannaFragorAccordion" ).addClass( "hiddenAccordion" );
+        $( "#yhAccordion" ).addClass( "hiddenAccordion" );
+        $( "#skolanAccordion" ).addClass( "hiddenAccordion" );
+        $( "#csnAccordion" ).removeClass( "hiddenAccordion" );
+        $( "#hurDuAnsokerAccordion" ).addClass( "hiddenAccordion" );
+    });
+} );
+
+// Visa enbart "Hur du ansöker"
+$( function() {
+    $( "#hurDuAnsoker" ).on( "click", function() {
+        $( "#almannaFragorAccordion" ).addClass( "hiddenAccordion" );
+        $( "#yhAccordion" ).addClass( "hiddenAccordion" );
+        $( "#skolanAccordion" ).addClass( "hiddenAccordion" );
+        $( "#csnAccordion" ).addClass( "hiddenAccordion" );
+        $( "#hurDuAnsokerAccordion" ).removeClass( "hiddenAccordion" );
+    });
+} );
