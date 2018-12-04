a0cc694f7f77cdc95e0ed373b4ad1dfbcbcdaf67
Pär Nilsson <95parnil@mk.se>
2018-12-04 11:16:20 +0100

    Fix på länkar i footer

--- webbprojekt/script/footer.js
+++ webbprojekt/script/footer.js
@@ -10,7 +10,7 @@ xhr1.onload = function () {
     newfooter += '<h4>' + responseNews.kontaktHeader +'</h4>';
 
     for (i in responseNews.kontaktLinks) {
-        newfooter+= '<li class="footer-item"><a' + responseNews.kontaktLinks[i] + '><i class="' + responseNews.kontaktPicture[i] + '"></i>' + responseNews.kontaktText[i] +'</a></li>';
+        newfooter+= '<li class="footer-item"><a href="' + responseNews.kontaktLinks[i] + '"><i class="' + responseNews.kontaktPicture[i] + '"></i>' + responseNews.kontaktText[i] +'</a></li>';
     }
     newfooter += '</ul>';
 
